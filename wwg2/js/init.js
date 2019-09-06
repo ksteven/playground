22f3e52d5910ad5f478f31aad163a66601e4a8e6
ksteven <stevenkhaouli@gmail.com>
2019-09-05 22:01:16 -0400

    add column text filtering

--- wwg2/js/init.js
+++ wwg2/js/init.js
@@ -156,9 +156,28 @@ $(document).ready(function () {
     return row;
   }
   function makeTable(jsonData) {
+
+    // Setup - add a text input to each footer cell
+    $('#example thead tr').clone(true).appendTo('#example thead');
+    $('#example thead tr:eq(1) th').each(function (i) {
+      var title = $(this).text();
+      $(this).html('<input type="text" placeholder="Filter ' + title + '" />');
+
+      $('input', this).on('keyup change', function () {
+        if (table.column(i).search() !== this.value) {
+          table
+            .column(i)
+            .search(this.value)
+            .draw();
+        }
+      });
+    });
+
     return $('#example').DataTable({
       //"ajax": 'https://ksteven.github.io/playground/wwg2/js/data.txt',
       "dom": '<f<t>ip>',
+      "orderCellsTop": true,
+      "fixedHeader": true,
       "language": {
         "search": "Filter"
       },
