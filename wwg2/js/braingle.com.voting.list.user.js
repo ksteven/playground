81c3bf196e52c8750d5cc749b13612792b806bd8
ksteven <stevenkhaouli@gmail.com>
2019-11-04 17:50:48 -0500

    add vote script

---
+++ wwg2/js/braingle.com.voting.list.user.js
@@ -0,0 +1,115 @@
+// ==UserScript==
+// @name     Voting List Automate
+// @version  1
+// @grant    none
+// @include        http://www.braingle.com/*
+// @include        https://www.braingle.com/*
+// @require https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js 
+// ==/UserScript==
+
+// add button
+if ($('input[name="vote"]').length > 0) { // check if active round
+    // console.log('active round, add button');
+    $('#main').find('.boxed_body > h2').eq(0).after('<p> <input id="doVLCopy" type="submit" value="Copy Voting List" class="button_primary t3"> <span id="result"></span></p>');
+};
+const colors = ["red", "blue", "green", "orange", "purple", "brown", "yellow", "lime", "aqua", "lilac", "navy"]
+
+// utility functions 
+const count = names => names.reduce((a, b) => ({
+    ...a,
+    [b]: (a[b] || 0) + 1
+}), {}) // don't forget to initialize the accumulator
+
+function fallbackCopyTextToClipboard(text, cb) {
+    var textArea = document.createElement("textarea");
+    textArea.value = text;
+    textArea.style.position = "fixed"; //avoid scrolling to bottom
+    document.body.appendChild(textArea);
+    textArea.focus();
+    textArea.select();
+    try {
+        var successful = document.execCommand('copy');
+        var msg = successful ? 'successful' : 'unsuccessful';
+        console.log('Fallback: Copying text command was ' + msg);
+        cb(successful);
+    } catch (err) {
+        console.error('Fallback: Oops, unable to copy', err);
+        cb(false);
+    }
+    document.body.removeChild(textArea);
+}
+
+function copyTextToClipboard(text, cb) {
+    if (!navigator.clipboard) {
+        fallbackCopyTextToClipboard(text, cb);
+        return;
+    }
+    navigator.clipboard.writeText(text).then(function () {
+        console.log('Async: Copying to clipboard was successful!');
+        cb(true);
+    }, function (err) {
+        console.error('Async: Could not copy text: ', err);
+        cb(false);
+    });
+}
+// end utility functions 
+
+$("#doVLCopy").click(function () {
+    var vlList = doVLCopy();
+    console.log(vlList);
+    copyTextToClipboard(vlList, function (succ) {
+        if (succ) {
+            $("#result").html("Successfully copied!")
+            $("#result").css('color', 'green');
+        } else {
+            $("#result").html("You must copy manually")
+            $("#result").css('color', 'red');
+            $("#result").append('<br/><textarea  onClick="this.select();" rows="4" cols="50">' + vlList + '</textarea>');
+        };
+    });
+});
+
+function doVLCopy() {
+    var div_html = $('#main').find('.boxed_body').html();
+    var splits = div_html.split("<br>");
+    var add_this = false;
+    var votes = [];
+    var votes_for = [];
+    for (var s in splits) {
+        if (splits[s].length === 0) { // only start/stop adding when we find a blank
+            add_this = !add_this;
+        }
+        if (add_this && splits[s].length > 0) {
+            votes.push(splits[s]);
+            var voted_for = splits[s].split(" ")[3];
+            votes_for.push(voted_for);
+        }
+    }
+    var count_votes = count(votes_for);
+    var players = [];
+    for (const [key, value] of Object.entries(count_votes)) {
+        if (value > 1) {
+            var player = { 'name': key, 'num': value };
+            players.push(player);
+        }
+    };
+    // sort Players desc by num votes
+    players.sort(function (a, b) {
+        return parseInt(b.num) - parseInt(a.num);
+    });
+    var countstr = "";
+    for (p in players) {
+        player = players[p];
+        countstr += "[color=" + colors[p] + "]" + player.name + " : " + player.num + "[/color]" + "\n";
+    };
+    for (v in votes) {
+        var vote = votes[v];
+        var voted_for = vote.split(" ")[3];
+        for (p in players) {
+            if (voted_for === players[p].name) { // add color
+                votes[v] = "[color=" + colors[p] + "]" + vote + "[/color]"
+            }
+        }
+    }
+    return votes.join('\n') + "\n \n" + "<b>Totals</b>" + "\n" + countstr;
+};
