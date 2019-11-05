5c81f42750c11218ee73f6edf6522c63af0a3005
ksteven <stevenkhaouli@gmail.com>
2019-11-05 13:12:36 -0500

    Update braingle.com.voting.list.user.js

--- wwg2/js/braingle.com.voting.list.user.js
+++ wwg2/js/braingle.com.voting.list.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name     Voting List Automate
-// @version  2.2
+// @version  2.2.0
 // @grant    none
 // @include        http://www.braingle.com/games/werewolf/game.php?id=*
 // @include        https://www.braingle.com/games/werewolf/game.php?id=*
