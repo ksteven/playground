aa449dd1fc776ec85cc522a1d7d616d64d13494b
ksteven <stevenkhaouli@gmail.com>
2021-11-01 16:18:24 -0400

    newhashtag

--- espn/HASHTAGCOMPARE.user.js
+++ espn/HASHTAGCOMPARE.user.js
@@ -1,10 +1,11 @@
 // ==UserScript==
 // @name         HASHTAG - COMPARE
 // @namespace    http://tampermonkey.net/
-// @version      0.2
+// @version      0.4
 // @description  team comparison tool
 // @author       You
 // @match        https://hashtagbasketball.com/*
+// @match        https://fantasy.espn.com/basketball/league/*
 // @icon         https://www.google.com/s2/favicons?domain=hashtagbasketball.com
 // @grant        none
 // @require https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js
@@ -14,11 +15,19 @@
 
 (function () {
     'use strict';
+    // espn page
+    const header = $($(".header")[0]).text().trim()
+    console.log("header is ", header);
+    if (header === "League Rosters" && $(".subHeader")) {
+        rosterPageLoad();
+    }
+
+    // hashtag basketball page
     let testmap = new Map(JSON.parse(localStorage.getItem('rosterMap')));
     console.log(testmap)
     if (testmap.size === 0) { // no rosters loaded
         // USE default 
-        let mystr = `[["Captain Pingu  ",["Damian Lillard","Donovan Mitchell","Buddy Hield","Anthony Davis","Rudy Gobert","Draymond Green","Mike Conley","Kevin Porter Jr.","Robert Covington","Lauri Markkanen","Joe Harris","Al Horford","Bojan Bogdanovic","Ben Simmons","Dillon Brooks"]],["Wet Like  I'm Book",["Chris Paul","Devin Booker","Andrew Wiggins","Jayson Tatum","Joel Embiid","Tim Hardaway Jr.","Scottie Barnes","Clint Capela","Jarrett Allen","Jonas Valanciunas","Malik Beasley","T.J. McConnell","Aaron Gordon"]],["We The North Masaiah 😇✌️",["Stephen Curry","D'Angelo Russell","John Collins","De'Aaron Fox","Derrick White","Mikal Bridges","Kemba Walker","Brandon Ingram","Thaddeus Young","Darius Bazley","Mo Bamba","Daniel Gafford","Josh Giddey","Bam Adebayo","Jaren Jackson Jr."]],["WhattA Joke",["Trae Young","Zach LaVine","Jimmy Butler","Tobias Harris","Nikola Jokic","Tyrese Haliburton","Robert Williams III","Jusuf Nurkic","Harrison Barnes","Jordan Clarkson","Evan Fournier","Kyle Anderson","Joe Ingles","Gordon Hayward","Nerlens Noel"]],["Vucci Mane",["Ja Morant","Malcolm Brogdon","Khris Middleton","Giannis Antetokounmpo","Nikola Vucevic","Kyle Lowry","Domantas Sabonis","Spencer Dinwiddie","Isaiah Stewart","Bogdan Bogdanovic","Saddiq Bey","Eric Bledsoe","Brook Lopez"]],["Toronto Dan's Team",["Luka Doncic","Shai Gilgeous-Alexander","Anthony Edwards","Christian Wood","Jakob Poeltl","LaMelo Ball","Gary Trent Jr.","Myles Turner","Cade Cunningham","Tyler Herro","Kyle Kuzma","Dennis Schroder","De'Andre Hunter","Klay Thompson","Mitchell Robinson"]],["Shaqtin  A Fool ",["LeBron James","DeMar DeRozan","Kevin Durant","Pascal Siakam","Deandre Ayton","Marcus Smart","RJ Barrett","Richaun Holmes","Darius Garland","Victor Oladipo","Terrence Ross","Serge Ibaka","Duncan Robinson"]],["SterlingHeights Uncle Vaxxers",["Russell Westbrook","CJ McCollum","Paul George","Michael Porter Jr.","Montrezl Harrell","Jordan Poole","Jerami Grant","Collin Sexton","Wendell Carter Jr.","Bradley Beal","Marvin Bagley III","Reggie Jackson","Evan Mobley","Kyrie Irving"]],["Don Bodmon",["Dejounte Murray","Fred VanVleet","Jaylen Brown","OG Anunoby","Karl-Anthony Towns","Devonte' Graham","Caris LeVert","Jalen Suggs","P.J. Washington","Nickeil Alexander-Walker","Daniel Theis","Isaiah Roby","Killian Hayes","Chris Boucher","Jonathan Isaac"]],["YYZ Chin Diesel ",["Jrue Holiday","James Harden","Keldon Johnson","Julius Randle","Kristaps Porzingis","Lonzo Ball","Norman Powell","Jalen Green","Miles Bridges","Kelly Olynyk","Otto Porter Jr.","Larry Nance Jr.","Jaden McDaniels","Zion Williamson","Terry Rozier"]]]`
+        let mystr = `[["WhattA Joke",["Trae Young","Zach LaVine","Gordon Hayward","Robert Williams III","Jusuf Nurkic","Evan Fournier","Nikola Jokic","Tobias Harris","Will Barton","Jimmy Butler","Tyrese Haliburton","Harrison Barnes","Daniel Gafford"]],["SterlingHeights Uncle Vaxxers",["Russell Westbrook","Jordan Clarkson","Michael Porter Jr.","Evan Mobley","Montrezl Harrell","CJ McCollum","Jerami Grant","Bradley Beal","Collin Sexton","Wendell Carter Jr.","Paul George","Reggie Jackson","Jordan Poole","Kyrie Irving"]],["Wet Like  I'm Book",["Chris Paul","Devin Booker","Scottie Barnes","Jayson Tatum","Joel Embiid","T.J. McConnell","Clint Capela","Jarrett Allen","Aaron Gordon","Andrew Wiggins","Tim Hardaway Jr.","Jonas Valanciunas","Grayson Allen"]],["Don Bodmon",["Cole Anthony","Fred VanVleet","Cam Reddish","Chris Boucher","Karl-Anthony Towns","Dejounte Murray","P.J. Washington","Jaylen Brown","OG Anunoby","Jalen Suggs","Nickeil Alexander-Walker","Devonte' Graham","Hassan Whiteside","Caris LeVert","Jonathan Isaac"]],["Toronto Dan's Team",["Luka Doncic","Shai Gilgeous-Alexander","Anthony Edwards","Myles Turner","Jakob Poeltl","LaMelo Ball","Kyle Kuzma","Mitchell Robinson","Dennis Schroder","Gary Trent Jr.","Christian Wood","Cade Cunningham","Tyler Herro","Klay Thompson"]],["Captain Pingu  ",["Damian Lillard","Donovan Mitchell","Buddy Hield","Anthony Davis","Rudy Gobert","Ricky Rubio","Robert Covington","Draymond Green","Lauri Markkanen","Al Horford","Mike Conley","Kevin Porter Jr.","Bojan Bogdanovic","Ben Simmons","Dillon Brooks"]],["Vucci Gang",["Ja Morant","Tyrese Maxey","Saddiq Bey","Domantas Sabonis","Nikola Vucevic","Malcolm Brogdon","Giannis Antetokounmpo","Spencer Dinwiddie","Bogdan Bogdanovic","Eric Bledsoe","Khris Middleton","Kyle Lowry","Isaiah Stewart","Brook Lopez"]],["We The North Masaiah 😇✌️",["D'Angelo Russell","Mikal Bridges","Brandon Ingram","Mo Bamba","Josh Giddey","John Collins","Jaren Jackson Jr.","Derrick White","Stephen Curry","De'Aaron Fox","Steven Adams","Mason Plumlee","Franz Wagner","Bam Adebayo"]],["YYZ Chin Diesel ",["Lonzo Ball","Terry Rozier","Norman Powell","Miles Bridges","Alperen Sengun","Kemba Walker","Julius Randle","Keldon Johnson","Alex Caruso","Desmond Bane","James Harden","Kristaps Porzingis","Jalen Green","Zion Williamson","Jrue Holiday"]],["Shaqtin  A Fool ",["Seth Curry","Darius Garland","Kevin Durant","Richaun Holmes","Deandre Ayton","Terrence Ross","Kelly Oubre Jr.","DeMar DeRozan","RJ Barrett","Marcus Smart","LeBron James","Serge Ibaka","Duncan Robinson","Pascal Siakam","Victor Oladipo"]]]`
         testmap = new Map(JSON.parse(mystr))
     }
     let keys = Array.from(testmap.keys());
@@ -81,16 +90,42 @@
         let teamid = "#" + id + "-table"
         $(teamid).html(`<table class="table table-bordered"><tbody>${thead}</tbody></table>`)
         let players = testmap.get($(this).val())
-        //console.log(players);
-        //console.log()
         for (let p of players) {
             $("#" + id + "-table > table > tbody").append("<tr>" + playerMap.get(p) + "</tr>");
         }
     })
-    $("#ContentPlaceHolder1_GridView1").find('tr').each(function (row) {
-        let playername = ""
-        var mytd = $(this).find("td")[2]
-        playername = $.trim($(mytd).text())
-        playerMap.set(playername, $.trim($(this).html()))
-    });
+    setTimeout(function () {
+        $("#ContentPlaceHolder1_GridView1").find('tr').each(function (row) {
+            let playername = ""
+            var mytd = $(this).find("td")[1]
+            playername = $.trim($(mytd).text())
+            playerMap.set(playername, $.trim($(this).html()))
+        });
+    }, 3000);
+    // console.log(playerMap)
+
+    function rosterPageLoad() {
+        return setTimeout(function () {
+            $(".subHeader").html('<button id="copyRosters" class="Button Button--alt Button--custom autob">Save Roster List</button>')
+            $("#copyRosters").click(function () {
+                const rosterMap = new Map()
+                $(".ResponsiveTable").each(function (r) {
+                    let team = $(this).find(".teamName").text()
+                    let players = []
+                    let playersAnchor = $(this).find(".player-column__athlete > .truncate > a").each(function (p) {
+                        if ($(this).text().length > 0) {
+                            players.push($(this).text())
+                        }
+                    })
+                    rosterMap.set(team, players)
+                })
+                let stringRoster = JSON.stringify(Array.from(rosterMap.entries()))
+                localStorage.setItem('rosterMap', stringRoster);
+                let testmap = new Map(JSON.parse(localStorage.getItem('rosterMap')));
+                navigator.clipboard.writeText(stringRoster)
+                $(this).parents(".container").append(`<p><textarea rows=10 style="width:100%">${stringRoster}</textbox></p>`)
+            })
+
+        }, 2000)
+    }
 })();
